fb242e01eb96c0658336bf1618d8aca1359e5b86
Luis Alberto Perez <luisalbertoperez@gmail.com>
2025-05-05 05:45:34 -0300

    incluida ip de do

--- src/django_docker_demo/settings.py
+++ src/django_docker_demo/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-1he$0-%v1i$(1!+$yt&6*#6(j@q1+k+mq36o@9!7z@x8^2xnex"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "24.199.117.200"]
 
 
 # Application definition
